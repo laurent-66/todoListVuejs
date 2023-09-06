18c20bd79c88c1ee281b9e9b1e9cc00137b8e0ff
Laurent LESAGE <lesageduweb@gmail.com>
2023-09-06 11:10:04 +0200

    first commit datafake + display list and delete button

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<script>
+  import ToDoList from './components/ToDoList.vue';
+
+  export default {
+    components: {
+        ToDoList
+      },
+
+    created() {
+      this.todos = [
+        {
+          id:1,
+          text: "Acheter du soda",
+          complete: false,
+          date: new Date().toDateString,
+          dateCompleted: null
+        },
+        {
+          id:2,
+          text: "Préparer la salade",
+          complete: false,
+          date: new Date().toDateString,
+          dateCompleted: null
+        },
+        {
+          id:3,
+          text: "Acheter le poulet",
+          complete: false,
+          date: new Date().toDateString,
+          dateCompleted: null
+        },
+        {
+          id:4,
+          text: "Préparer une tarte aux pommes",
+          complete: false,
+          date: new Date().toDateString,
+          dateCompleted: null
+        },
+        {
+          id:5,
+          text: "Prendre le matériel pour le pique-nique",
+          complete: false,
+          date: new Date().toDateString,
+          dateCompleted: null
+        },
+      ]
+    },
+    methods: {
+      deleteTodo(todoId) {
+        if(confirm('est vous sûr de supprimer?')) {
+          this.todos = this.todos.filter((todo)=> todo.id !== todoId)
+        }
+      }
+    }
+  }
+</script>
+
+<template>
+  <div>
+      <ToDoList :todos="todos" @delete-todo="deleteTodo" />
+  </div>
+
+</template>
+
+<style scoped>
+
+
+@media (min-width: 1024px) {
+
+}
+</style>
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,43 @@
+<script>
+  export default {
+    props: {
+      todos: Array
+    },
+    emits: ['delete-todo']
+  }
+</script>
+
+<template>
+  <div>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">A faire</th>
+          <th scope="col">Date de création</th>
+          <th scope="col">Tâche effectué</th>
+          <th scope="col">Editer</th>
+          <th scope="col">Supprimer</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="todo in todos" :key="todo.id">
+          <th scope="row">{{ todo.text }}</th>
+          <td>{{ todo.date }}</td>
+          <td>tâche complète</td>
+          <td>Editer</td>
+          <td>
+            <button @click="$emit('delete-todo, todo.id')" class="btn btn-danger">Supprimer</button>
+          </td>          
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<style scoped>
+
+
+@media (min-width: 1024px) {
+
+}
+</style>
